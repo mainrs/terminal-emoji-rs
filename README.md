ebf7167944cb988e0db324b1a9f996daa215e8af
SirWindfield <SirWindfield@users.noreply.github.com>
2021-03-04 00:06:28 +0100

    chore: first implementation

---
+++ README.md
@@ -0,0 +1,21 @@
+# terminal-emoji
+
+> A Rust library for safely displaying emojis inside terminals.
+
+> **Note:** The code is taken from `console` and put into its own crate.
+
+## License
+
+Licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or
+  https://www.apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or https://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dua
---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+use crossterm::tty::IsTty;
+use std::fmt;
+use std::io::stdout;
+
+#[cfg(not(target_os = "macos"))]
+lazy_static::lazy_static! {
+    static ref IS_LANG_UTF8: bool = {
+        match std::env::var("LANG") {
+            Ok(lang) => lang.to_uppercase().ends_with("UTF-8"),
+            _ => false,
+        }
+    };
+}
+
+pub struct Emoji<'a, 'b>(pub &'a str, pub &'b str);
+
+impl<'a, 'b> Emoji<'a, 'b> {
+    pub fn new(emoji: &'a str, fallback: &'b str) -> Self {
+        Self(emoji, fallback)
+    }
+}
+
+impl fmt::Display for Emoji<'_, '_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if stdout().is_tty() && is_emoji_supported() {
+            write!(f, "{}", self.0)
+        } else {
+            write!(f, "{}", self.1)
+        }
+    }
+}
+
+#[cfg(windows)]
+fn is_emoji_supported() -> bool {
+    std::env::var("WT_SESSION").is_some()
+}
+
+#[cfg(target_os = "macos")]
+fn is_emoji_supported() -> bool {
+    true
+}
+
+#[cfg(not(target_os = "macos"))]
+fn is_emoji_supported() -> bool {
+    true
+}
