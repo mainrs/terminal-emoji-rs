0d38adf6dec587a9bc8c763061c51a69489a11d2
SirWindfield <SirWindfield@users.noreply.github.com>
2021-03-04 01:58:31 +0100

    refactor: use atty instead of crossterm

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-use crossterm::tty::IsTty;
+use atty::Stream;
 use std::fmt;
-use std::io::stdout;
 
 #[cfg(not(target_os = "macos"))]
 lazy_static::lazy_static! {
@@ -22,7 +21,7 @@ impl<'a, 'b> Emoji<'a, 'b> {
 
 impl fmt::Display for Emoji<'_, '_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if stdout().is_tty() && is_emoji_supported() {
+        if should_display_emoji() {
             write!(f, "{}", self.0)
         } else {
             write!(f, "{}", self.1)
@@ -30,16 +29,28 @@ impl fmt::Display for Emoji<'_, '_> {
     }
 }
 
+// Emojis should only get displayed if the current terminal is a tty and the
+// platform does support emojis.
+fn should_display_emoji() -> bool {
+    atty::is(Stream::Stdout) && is_emoji_supported()
+}
+
+// The new Windows Terminal does support emojis. Currently, the terminal will
+// set the environment variable `WT_SESSION`. This can be used to check if the
+// user uses that specific app.
 #[cfg(windows)]
 fn is_emoji_supported() -> bool {
     std::env::var("WT_SESSION").is_some()
 }
 
+// macOS by default has emoji support.
 #[cfg(target_os = "macos")]
 fn is_emoji_supported() -> bool {
     true
 }
 
+// On unix systems the enabled language decides whether emojis are supported or
+// not.
 #[cfg(not(target_os = "macos"))]
 fn is_emoji_supported() -> bool {
     *IS_LANG_UTF8
